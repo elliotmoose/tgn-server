de889f6464944ebff940d389a4c7ab4eebfa4623
Elliot Koh <appdev.moose@gmail.com>
2020-10-04 17:31:47 +0800

    removed typescript

--- src/controllers/user/index.js
+++
@@ -1,6 +0,0 @@
-import makeSignUp from "./signup";
-
-//use cases
-import { createUser } from "../../use-cases/user";
-
-export const signup = makeSignUp({ createUser });
--- src/controllers/user/signup.js
+++
@@ -1,7 +0,0 @@
-export default function makeSignUp ({ createUser }) {
-    return async function signUp(httpReq) {
-        const userData = httpReq.body;
-        const newUser = await createUser(userData);
-        return newUser;
-    }
-}
